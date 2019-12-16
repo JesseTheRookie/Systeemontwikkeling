2d087b50e6676099dd2463b08f1f85415740c4d8
Arif KEmal <550122@student.inholland.nl>
2019-12-16 14:36:04 +0100

    contact aangepast

--- app/DAL/function.php
+++ app/DAL/function.php
@@ -1,11 +1,6 @@
 <?php 
 class Contact{
-    private $host="localhost";
-    private $user="root";
-    private $pass="";
-    private $db="contact";
-    public $mysqli;
-    
+
     public function __construct() {
         return $this->mysqli=new mysqli($this->host, $this->user, $this->pass, $this->db);
     }
@@ -16,10 +11,10 @@ class Contact{
         $email=$data['email'];
         $phone=$data['phone'];
         $message=$data['message'];
-        $q="insert into contact_us set first_name='$fname', last_name='$lname', email='$email', phone='$phone', message='$message'";
+        $q="insert into contact_us set first_name='$firstname', last_name='$lastname', email='$email', phone='$phone', message='$message'";
        $data= $this->mysqli->query($q);
        if($data==true){
-           $body="One message received from Haarlem Festival contact us. details are below..<br> first_name='$fname', last_name='$lname', email='$email', phone='$phone', message='$message'";
+           $body="One message received from Haarlem Festival contact us. details are below..<br> first_name='$firstname', last_name='$lastname', email='$email', phone='$phone', message='$message'";
            return $this->sent_mail("a.k.cagdas@live.nl", "Message received from Haarlem Festival", $body);
        }
     }
