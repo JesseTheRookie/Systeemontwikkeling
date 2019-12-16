4ab2d902656c30154064e01c7b1e097b510a7d4b
Jesse <Email>
2019-12-16 12:40:07 +0100

    Deze aanpassingen vergeten te pushen

--- app/DAL/JazzTicketDAO.php
+++ app/DAL/JazzTicketDAO.php
@@ -36,9 +36,9 @@ class JazzTicketDAO{
 
             array_push($jazzTicketArray, $jazzTicketModel);
         }
-
         return $jazzTicketArray;
     }
+
     public function getArtists(){
 
         $artistArray = array();
@@ -61,52 +61,6 @@ class JazzTicketDAO{
 
             array_push($artistArray, $artistModel);
         }
-
         return $artistArray;
     }
-
-    /*public function getAllJazzTickets(){
-        $conn = new mysqli('localhost', 'root', '', 'haarlem_festival' );
-s
-        $sql = "SELECT t.ticketId, t.startDateTime, t.endDateTime, t.status, t.ticketQuantity, t.price, j.jazzTicketLocation, j.jazzTicketHall, a.artistName, a.artistBio, a.eventType, a.artistId
-                FROM tickets AS t
-                INNER JOIN jazzticket AS j 
-                ON t.ticketId = j.ticketId
-                INNER JOIN performancejazz AS p
-                ON j.ticketId = p.JazzTicketId
-                INNER JOIN artist AS a 
-                ON p.JazzArtist = a.artistId";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-          while($row = $result->fetch_assoc()) {
-
-            $jazzTicket = new JazzTicketModel();
-            $artist = new ArtistModel();
-
-            $jazzTicket->setJazzTicketId($row["j.ticketId"]);
-            $jazzTicket->setJazzTicketLocation($row["j.jazzTicketLocation"]);
-            $jazzTicket->setJazzTicketHall($row["j.jazzTicketHall"]);
-            $jazzTicket->setEndDateTime($row["t.endDateTime"]);
-            $jazzTicket->setStartDateTime($row["t.startDateTime"]);
-            $jazzTicket->setTicketQuantity($row["t.ticketQuantity"]);
-            $jazzTicket->setPrice($row["t.price"]);
-            $jazzTicket->setStatus($row["t.status"]);
-
-            $artist->setArtistId($row["a.artistId"]);
-            $artist->setArtistName($row["a.artistName"]);
-            $artist->setArtistBio($row["a.artistBio"]);
-            $artist->setEventType($row["a.eventType"]);
-
-            $jazzTicket->addArtist($artist);
-
-            array_push($this->jazzTicketList, $jazzTicket);
-          }
-        return $this->jazzTicketList;
-      } else {
-          echo "0 results";
-      }
-      $conn->close();
-    }
-    */
 }
