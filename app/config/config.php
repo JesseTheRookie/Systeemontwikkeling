1e6e3724df6ea4981af45714d8552f1137cad415
Dary <623530@student.inholland.nl>
2020-01-26 13:03:00 +0100

    Config aangepast.

--- app/config/config.php
+++ app/config/config.php
@@ -5,16 +5,16 @@
   //define('DB_PASS', 'Phpiskut1!');
   //define('DB_NAME', 'md500698db492431');
 
-  define('DB_HOST', 'db.nazaragency.nl');
-  define('DB_USER', 'md500698db492431');
-  define('DB_PASS', 'Phpiskut1!');
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
   define('DB_NAME', 'md500698db492431');
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
 
   // URL Root
-  define('URLROOT', 'http://www.darynazar.com/Systeemontwikkeling');
+  define('URLROOT', 'http://localhost/Systeemontwikkeling');
 
   // Site Name
   define('SITENAME', 'Haarlem Festival');
